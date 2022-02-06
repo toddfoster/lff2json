38a81c413c11aa02061cc6cc68d2669fa2e21237
Todd Foster <toddfoster@users.noreply.github.com>
2022-02-05 21:32:40 -0500

    Parsing index

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # lff2json
 Parse Lesser Feasts and Fasts 2018 into json
+
+- LFF2018 is available online in pdf form.
+- pdftotext gives me plain text.
+- Now I want to parse it into json for automated use by www.sharedprayers.net.
+- Found dates, bio's, maybe collects in venite.app, but not scriptures.
+
+# Process:
+- x Find page separators: pdf has 584pp.: "\f"
+- Parse index to LFF2018 for authoritative date & name of each feast (Pp 7--18)
+- How to choose slugs? Halfway manual?
+	- Find main page for each feast
+	- preceding page is bio
+	- page with name at top has two collects, scriptures, preface
--- lff2json.py
+++ lff2json.py
@@ -4,7 +4,9 @@ page = 0
 index_start = 14
 index_end = 25
 characters = 0
-
+mayjune_day = 0
+BLANK_DAYS_IN_MAY = [6, 7, 10, 12, 14, 16, 18, 23, 27, 29, 30]
+BLANK_DAYS_IN_JUNE = [6, 7, 13, 20, 21, 23, 25, 27, 30]
 months = []
 for m in month_name[1:]:
     months += [m.upper()]
@@ -16,6 +18,13 @@ def print_hex_chars(line_in):
         print(f"{c}: {ord(c):02x}")
 
 
+def find_feast_by_date(feasts, mm, dd):
+    for f in feasts:
+        if f["mm"] == mm and f["dd"] == dd:
+            return f
+
+
+feasts = []
 with open("src/lff2018.txt", "r", encoding="utf-8") as f:
     current_month = 0
     previous_record = {}
@@ -31,13 +40,32 @@ with open("src/lff2018.txt", "r", encoding="utf-8") as f:
         if index_start <= page and page <= index_end:
             l = line.strip().replace("\x07", "")
 
+            if len(l) < 3:
+                continue
+
             # Check for new month name
             if l in months:
-                current_month = months.index(l) + 1
-                print(f"current_month={current_month}")
+                current_month = f"{months.index(l)+1:02}"
+                # print(f"current_month={current_month}")
+                mayjune_day = 0
                 continue
-            if current_month < 0:
-                continue  # just in case
+
+            # May and June are messed up & need special treatment
+            if current_month in ('05', '06'):
+                mayjune_day += 1
+                while current_month == "05" and mayjune_day in BLANK_DAYS_IN_MAY:
+                    mayjune_day += 1
+                while current_month == "06" and mayjune_day in BLANK_DAYS_IN_JUNE:
+                    mayjune_day += 1
+                if mayjune_day > 31:
+                    continue
+                feasts.append({
+                    "mm": current_month,
+                    "dd": f"{mayjune_day:02}",
+                    "title": l,
+                })
+                print(f" found {current_month}/{mayjune_day}: {l}")
+                continue
 
             # parse line in month -- sometimes lines have a linebreak!
             splits = l.split(" ", maxsplit=1)
@@ -46,10 +74,27 @@ with open("src/lff2018.txt", "r", encoding="utf-8") as f:
             day = splits[0]
             feast = splits[1].strip()
             if day.isnumeric() and int(day) < 32:
-                print(f"{int(current_month):02}/{int(day):02}: {feast}")
+                current_day = f"{int(day):02}"
+                # print(f"{current_month}/{day}: {feast}")
+                previous_record = {
+                    "mm": current_month,
+                    "dd": current_day,
+                    "title": feast,
+                }
+                feasts.append(previous_record)
+            else:
+                previous_record["title"] += " " + l
             continue
 
         # Past index
         print("past index")
         break
 print("{} pages processed".format(page))
+print(find_feast_by_date(feasts, "04", "14"))
+print(find_feast_by_date(feasts, "05", "17"))
+print(find_feast_by_date(feasts, "05", "31"))
+print(find_feast_by_date(feasts, "06", "15"))
+print(find_feast_by_date(feasts, "06", "29"))
+print("------------")
+for f in feasts:
+    print(f"{f['mm']}/{f['dd']}: {f['title']}")
